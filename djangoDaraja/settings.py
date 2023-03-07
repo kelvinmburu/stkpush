26f91839746032f25463b6f0b953131112d50abb
kelvinmburu <mburuhkelvin@gmail.com>
2023-03-07 18:18:30 +0300

    fx: fixed leaked keys

--- djangoDaraja/settings.py
+++ djangoDaraja/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ft2$3oku@rj9a4f^*)d(43f_858c8&!cg&!(zloy8#ivwx96mw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
